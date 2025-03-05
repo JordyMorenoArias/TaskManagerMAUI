28d6c2e59e5b87b048783865f54e35ce2a219fed
JordyMorenoArias <yordimorenoarias.11@gmail.com>
2025-03-05 18:44:03 -0400

    Add project files.
    At this point, all the pages have been created, and styles, modeling, and logic have been applied only to the Login and Home pages, allowing users to log in, view pending tasks, and mark them as completed.

---
+++ TaskManagerMAUI/Models/HomeModelViewData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TaskManagerMAUI.Models
+{
+    public class HomeModelViewData
+    {
+        public IEnumerable<Task>? Tasks { get; set; } = new List<Task>();
+    }
+}
